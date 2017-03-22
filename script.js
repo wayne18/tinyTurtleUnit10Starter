531194deaff09e83a25fb2a78968a739bbaa4757
Wayne Mok <mok@aclanyc.com>
2017-03-22 12:34:06 +0000

    TT new shape

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
 
-function square(){
+function square(s){
 forward(50);
 right(90);
 forward(50);
@@ -11,8 +11,37 @@ forward(50);
 right(90);
 forward(50);
 }
-
+function triangle(t){
+right(t);
+forward(t);
+right(t);
+forward(t);
+}
+function rectangle(r){
+forward(r);
+right(r);
+forward(r);
+right(r);
+forward(r);
+right(r);
+forward(r);
+right(r);
+forward(r);
+right(r);
+forward(r);
+right(r);
+forward(r);
+right(r);
+forward(r);
+right(r);
+forward(r);
+right(r);
+forward(r);
+forward(r);
+right(r);
+forward(r);
+}
 // Type your function call below
-
-
-stamp();
+square();
+triangle(156);
+rectangle(30);
